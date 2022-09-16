11c3e2f51d396b61e3bf58d6a3426d50b90d75ec
JTZ18 <jon.taylorlim99@gmail.com>
2022-09-16 09:56:46 +0800

    setup API user management

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,7 +12,7 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful."""
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(email, password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
